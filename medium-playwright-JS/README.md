32482c3d660aab066ae1fe46d30f67f666f9a3b9
Diego Magdaleno <dmmagdal@ucsc.edu>
2023-11-28 12:05:52 -0800

    Added support for inserting local image paths to 'alt' attribute of <img> tags.

--- medium-playwright-JS/README.md
+++ medium-playwright-JS/README.md
@@ -6,7 +6,7 @@ Description: A simple script that works to download and catalog medium articles 
 ### Requirements/Todos
 
 [ ] All images are saved as either png or svg
-[ ] Save (embedded) videos locally in AV1 compressed format
+[ ] Save (embedded) videos locally in AV1 compressed format <- AV1 is not a compression format, but an encoding! Investigate possible (lossless compressed) video formats
 [ ] Articles can be restored to a "Medium"-esque style (with a default CSS)
 [ ] Dockerize
 [ ] Map all article urls to their local folder hashes (and vice versa) in a JSON file (keep updated as well)
@@ -20,6 +20,8 @@ Description: A simple script that works to download and catalog medium articles 
 ### Notes
 
  - According to Google Chrome, the test Medium article ([Scraping Web Apps using Direct HTTP Request](https://medium.com/analytics-vidhya/scraping-web-apps-using-direct-http-request-f5c02a2874fe)) took about 150MB of memory in the browser while the local folder uses around 3MB of storage.
+ - At this time (Nov 2023), scraping all links (106 total) listed in `links.txt`, the data storage useage is 261MB on disk (around 2.46MB per link on average).
+     - NOTE: video saving has not been implemented yet. This will likely grow once that is integrated.
 
 
 ### References
@@ -27,3 +29,7 @@ Description: A simple script that works to download and catalog medium articles 
  - [ScrapingBee](https://www.scrapingbee.com/blog/playwright-web-scraping/): "Scraping the web with Playwright"
  - [ScrapingAnt](https://scrapingant.com/blog/playwright-download-file): "How to download a file with Playwright"
  - [Medium article](https://medium.com/@animeshsingh161/downloading-images-with-playwright-headless-browser-using-multithreading-python-b22d54311eda): "Downloading Images with playwright headless browser using Multithreading | Python"
+ - cli-progress [npm module](https://www.npmjs.com/package/cli-progress)
+ - cheerio [npm module](https://www.npmjs.com/package/cheerio)
+ - playwright [npm module](https://www.npmjs.com/package/playwright)
+ - playwright [documentation](https://playwright.dev/docs/intro)
--- medium-playwright-JS/index.js
+++ medium-playwright-JS/index.js
@@ -1,5 +1,7 @@
 // Import the necessary packages.
 const playwright = require('playwright');
+const cheerio = require('cheerio');
+const cliProgress = require('cli-progress');
 const crypto = require('crypto');
 const fs = require('fs');
 const https = require('https');
@@ -18,24 +20,25 @@ async function downloadImage(imageURL, imagePath) {
 // Create the folder structure for storing the data.
 function createFolders(urlHash) {
     // Create the base folder.
-    if (!fs.existsSync(urlHash) || !fs.statSync(urlHash).isDirectory()) {
-        fs.mkdirSync(urlHash);
+    const basePath = path.join('savedArticles', urlHash)
+    if (!fs.existsSync(basePath) || !fs.statSync(basePath).isDirectory()) {
+        fs.mkdirSync(basePath, {recursive: true});
     }
 
     // Create the images folder.
-    const imagesPath = path.join(urlHash, 'images');
+    const imagesPath = path.join(basePath, 'images');
     if (!fs.existsSync(imagesPath) || !fs.statSync(imagesPath).isDirectory()) {
         fs.mkdirSync(imagesPath);
     }
 
     // Create the videos folder.
-    const videosPath = path.join(urlHash, 'videos');
+    const videosPath = path.join(basePath, 'videos');
     if (!fs.existsSync(videosPath) || !fs.statSync(videosPath).isDirectory()) {
         fs.mkdirSync(videosPath);
     }
 
     // Return the paths of the main folder and subfolders.
-    return [urlHash, imagesPath, videosPath];
+    return [basePath, imagesPath, videosPath];
 }
 
 
@@ -77,7 +80,9 @@ async function scrapeArticle(page, url, saveOriginalHTML=true) {
     const images = await section.locator('img').all();
     for (let i = 0; i < images.length; i++) {
         const imgURL = await images[i].getAttribute('src');
-        const imgPath = path.join(imagesPath, `image_${i}.png`);    // TODO: make sure all images can be saved in PNG format.
+
+        // TODO: Make sure all images can be saved in PNG format.
+        const imgPath = path.join(imagesPath, `image_${i}.png`);    
         await downloadImage(imgURL, imgPath);
     }
 
@@ -90,10 +95,17 @@ async function scrapeArticle(page, url, saveOriginalHTML=true) {
         fs.writeFileSync(filePathOriginalCopy, sectionHTML);
     }
 
-    // TODO: Modify the HTML code (returned from 
-    // section.innerHTML()).
+    // Modify each <img> tag to include the local image path as the 
+    // 'alt' attribute. 
+    const $ = cheerio.load(sectionHTML);
+    $('img').each((index, element) => {
+        const imgPath = path.join(imagesPath, `image_${index}.png`); 
+        $(element).attr('alt', imgPath);
+    });
+
+    // Save the modified HTML.
     if (!fs.existsSync(filePath)) {
-        fs.writeFileSync(filePath, sectionHTML);
+        fs.writeFileSync(filePath, $.html());
     }
 }
 
@@ -118,15 +130,25 @@ async function main() {
     // Initialize new page in the browser and go to the target website.
     // const article = 'https://medium.com/analytics-vidhya/scraping-web-apps-using-direct-http-request-f5c02a2874fe';
     const page = await browser.newPage();
+
+    // Initialize CLI progress bar.
+    const scrapeBar = new cliProgress.SingleBar(
+        {},
+        cliProgress.Presets.shades_classic,
+    );
+    console.log(`Scraping articles from ${linksFile}`);
+    scrapeBar.start(addresses.length, 0);
     
     // Iterate through each address, scraping it if has not already been processed.
     for (const address of addresses) {
         await scrapeArticle(page, address);
+        scrapeBar.increment();
     }
+    scrapeBar.stop();
 
     // Close the browser.
     await browser.close();
 }
 
 
-main()
+main();
